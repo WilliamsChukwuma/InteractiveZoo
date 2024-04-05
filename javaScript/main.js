e5ccbffe09274592c9d559a818bf11497a8830fe
Williams Chukwuma <157719872+WilliamsChukwuma@users.noreply.github.com>
2024-04-04 23:04:14 -0400

    Add files via upload

---
+++ javaScript/main.js
@@ -0,0 +1,41 @@
+const factText = document.getElementById("factText");
+
+        document.getElementById("elephant").addEventListener("click", function() {
+            factText.textContent = "Elephants are the largest land animals in the world.";
+            factText.style.color = "aqua";
+        });
+
+        document.getElementById("tiger").addEventListener("click", function() {
+            factText.textContent = "Tigers are the largest cat species in the world.";
+            factText.style.color = "orange";
+        });
+
+        document.getElementById("giraffe").addEventListener("click", function() {
+            factText.textContent = "Giraffes have long necks to reach leaves high up in trees.";
+            factText.style.color = "blue";
+        });
+
+        document.getElementById("bat").addEventListener("click", function() {
+            factText.textContent = "Bats are the only mammals capable of sustained flight.";
+            factText.style.color = "red";
+        });
+
+        document.getElementById("owl").addEventListener("click", function() {
+            factText.textContent = "Owls don't have eyeballs.";
+            factText.style.color = "orange";
+        });
+
+        document.getElementById("jellyfish").addEventListener("click", function() {
+            factText.textContent = "Jellyfish can sink a boat.";
+            factText.style.color = "aqua";
+        });
+
+        document.getElementById("octopus").addEventListener("click", function() {
+            factText.textContent = "Octopus have three hearts.";
+            factText.style.color = "red";
+        });
+
+        document.getElementById("crow").addEventListener("click", function() {
+            factText.textContent = "Crows can remember faces 💀.";
+            factText.style.color = "yellow";
+        });
